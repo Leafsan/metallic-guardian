74dbe27f62f71d4f534beca5be07a8d16151b564
Leafsan <cwk0906@naver.com>
2024-09-02 22:50:31 +0900

    first commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const METALLIC_GUARDIAN = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+METALLIC_GUARDIAN.abilities = {
+  str: 'METALLIC_GUARDIAN.Ability.Str.long',
+  dex: 'METALLIC_GUARDIAN.Ability.Dex.long',
+  con: 'METALLIC_GUARDIAN.Ability.Con.long',
+  int: 'METALLIC_GUARDIAN.Ability.Int.long',
+  wis: 'METALLIC_GUARDIAN.Ability.Wis.long',
+  cha: 'METALLIC_GUARDIAN.Ability.Cha.long',
+};
+
+METALLIC_GUARDIAN.abilityAbbreviations = {
+  str: 'METALLIC_GUARDIAN.Ability.Str.abbr',
+  dex: 'METALLIC_GUARDIAN.Ability.Dex.abbr',
+  con: 'METALLIC_GUARDIAN.Ability.Con.abbr',
+  int: 'METALLIC_GUARDIAN.Ability.Int.abbr',
+  wis: 'METALLIC_GUARDIAN.Ability.Wis.abbr',
+  cha: 'METALLIC_GUARDIAN.Ability.Cha.abbr',
+};
